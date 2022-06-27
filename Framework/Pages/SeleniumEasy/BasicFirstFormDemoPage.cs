85c1b49b8d75c9f91498ca078f50cc9ae1af10d1
MartynasVCS <martynasvcs@gmail.com>
2022-06-27 19:33:05 +0300

    Add test for single input form demo

---
+++ Framework/Pages/SeleniumEasy/BasicFirstFormDemoPage.cs
@@ -0,0 +1,30 @@
+﻿namespace Framework.Pages.SeleniumEasy
+{
+    public class BasicFirstFormDemoPage
+    {
+        public static void closeAdvertisement()
+        {
+            string locator = "//*[@id='at-cv-lightbox-close']";
+            Common.waitForElementToBeVisible(locator);
+            Common.clickElement(locator);
+        }
+
+        public static void enterMessage(string message)
+        {
+            string locator = "//*[@id='user-message']";
+            Common.sendKeysToElement(locator, message);
+        }
+
+        public static void clickShowMessageButton()
+        {
+            string locator = "//*[@id='get-input']/button";
+            Common.clickElement(locator);
+        }
+
+        public static string readMessage()
+        {
+            string locator = "//*[@id='display']";
+            return Common.getElementText(locator);
+        }
+    }
+}
---
+++ Tests/SeleniumEasy/BasicFirstFormDemo.cs
@@ -0,0 +1,36 @@
+﻿using Framework;
+using Framework.Pages.SeleniumEasy;
+using NUnit.Framework;
+
+namespace Tests.SeleniumEasy
+{
+    public class BasicFirstFormDemo
+    {
+        [SetUp]
+        public static void setup()
+        {
+            Driver.setDriver();
+            Driver.open("https://demo.seleniumeasy.com/basic-first-form-demo.html");
+            BasicFirstFormDemoPage.closeAdvertisement();
+        }
+
+        [Test]
+        public static void singleInputField()
+        {
+            string expectedMessage = "Test";
+            string actualMessage;
+
+            BasicFirstFormDemoPage.enterMessage(expectedMessage);
+            BasicFirstFormDemoPage.clickShowMessageButton();
+            actualMessage = BasicFirstFormDemoPage.readMessage();
+
+            Assert.AreEqual(expectedMessage, actualMessage);
+        }
+
+        [TearDown]
+        public static void close()
+        {
+            Driver.closeDriver();
+        }
+    }
+}
