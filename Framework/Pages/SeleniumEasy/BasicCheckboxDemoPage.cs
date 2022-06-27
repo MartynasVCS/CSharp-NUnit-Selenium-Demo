ddab3b2fe8f332c56f9be0c3ab2dcefb660b25e9
MartynasVCS <martynasvcs@gmail.com>
2022-06-27 20:28:01 +0300

    Add single checkbox test

---
+++ Framework/Pages/SeleniumEasy/BasicCheckboxDemoPage.cs
@@ -0,0 +1,17 @@
+﻿namespace Framework.Pages.SeleniumEasy
+{
+    public class BasicCheckboxDemoPage
+    {
+        public static void clickSingleCheckbox()
+        {
+            string locator = "//*[@id='isAgeSelected']";
+            Common.clickElement(locator);
+        }
+
+        public static string readMessage()
+        {
+            string locator = "//*[@id='txtAge']";
+            return Common.getElementText(locator);
+        }
+    }
+}
---
+++ Tests/SeleniumEasy/BasicCheckboxDemo.cs
@@ -0,0 +1,34 @@
+﻿using Framework;
+using Framework.Pages.SeleniumEasy;
+using NUnit.Framework;
+
+namespace Tests.SeleniumEasy
+{
+    public class BasicCheckboxDemo
+    {
+        [SetUp]
+        public static void setup()
+        {
+            Driver.setDriver();
+            Driver.open("https://demo.seleniumeasy.com/basic-checkbox-demo.html");
+        }
+
+        [Test]
+        public static void singleCheckbox()
+        {
+            string expectedMessage = "Success - Check box is checked";
+            string actualMessage;
+
+            BasicCheckboxDemoPage.clickSingleCheckbox();
+            actualMessage = BasicCheckboxDemoPage.readMessage();
+
+            Assert.AreEqual(expectedMessage, actualMessage);
+        }
+
+        [TearDown]
+        public static void close()
+        {
+            Driver.closeDriver();
+        }
+    }
+}
