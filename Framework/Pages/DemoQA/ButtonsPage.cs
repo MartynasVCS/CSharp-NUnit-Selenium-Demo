94c9851981f1fefe18778e90a58b29bc7064b249
MartynasVCS <martynasvcs@gmail.com>
2022-06-27 21:12:33 +0300

    Add test for left button click

---
+++ Framework/Pages/DemoQA/ButtonsPage.cs
@@ -0,0 +1,17 @@
+﻿namespace Framework.Pages.DemoQA
+{
+    public class ButtonsPage
+    {
+        public static void leftClick()
+        {
+            string locator = "//*[text()='Click Me']";
+            Common.clickElement(locator);
+        }
+
+        public static string readMessageLeftClick()
+        {
+            string locator = "//*[@id='dynamicClickMessage']";
+            return Common.getElementText(locator);
+        }
+    }
+}
---
+++ Tests/DemoQA/Buttons.cs
@@ -0,0 +1,34 @@
+﻿using Framework;
+using Framework.Pages.DemoQA;
+using NUnit.Framework;
+
+namespace Tests.DemoQA
+{
+    public class Buttons
+    {
+        [SetUp]
+        public static void setup()
+        {
+            Driver.setDriver();
+            Driver.open("https://demoqa.com/buttons");
+        }
+
+        [Test]
+        public static void leftClick()
+        {
+            string expectedMessage = "You have done a dynamic click";
+            string actualMessage;
+
+            ButtonsPage.leftClick();
+            actualMessage = ButtonsPage.readMessageLeftClick();
+
+            Assert.AreEqual(expectedMessage, actualMessage);
+        }
+
+        [TearDown]
+        public static void close()
+        {
+            Driver.closeDriver();
+        }
+    }
+}
